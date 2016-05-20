1ed3efa1930b7a8a36756c5dfd7faa86ac80481f
Sam Hudgens <samhudgens@Zipcoder24s-MacBook-Pro.local>
2016-05-20 14:32:29 -0400

    Update some tests

--- src/test/java/com/samhudgens/BankSpec.java
+++ src/test/java/com/samhudgens/BankSpec.java
@@ -30,12 +30,49 @@ public class BankSpec {
         assertEquals("balance should be 800.0", expected, actual, 0);
     }
 
-//    @Test
-//    public void ClosedAccountWithdrawalTest() {
-//        bank.isClosed(bobsAccount);
-//        bank.withdrawal(200, bobsAccount);
-//        double expected = 1000.0;
-//        double actual = bobsAccount.getBalance();
-//        assertEquals("balance should remain the same", expected, actual, 0);
-//    }
+    @Test
+    public void ClosedAccountWithdrawalTest() {
+        bobsAccount.setAccountStatus(Account.AccountStatus.CLOSED);
+        bank.withdrawal(200, bobsAccount);
+        double expected = 1000.0;
+        double actual = bobsAccount.getBalance();
+        assertEquals("should deny withdrawal and balance should remain the same", expected, actual, 0);
+    }
+
+    @Test
+    public void OverDraftProtectionEnabledWithdrawalTest() {
+        bobsAccount.setOverdraftPrevention(Account.OverdraftPrevention.ENABLED);
+        bank.withdrawal(1200, bobsAccount);
+        double expected = 1000.0;
+        double actual = bobsAccount.getBalance();
+        assertEquals("should deny debit and keep balance the same", expected, actual, 0);
+    }
+
+    @Test
+    public void ClosedAccountDepositTest() {
+        bobsAccount.setAccountStatus(Account.AccountStatus.CLOSED);
+        bank.deposit(800, bobsAccount);
+        double expected = 1000;
+        double actual = bobsAccount.getBalance();
+        assertEquals("should deny deposit and keep balance the same", expected, actual, 0);
+    }
+
+    @Test
+    public void OpenAccountDepositTest() {
+        bank.deposit(800, bobsAccount);
+        double expected = 1800.0;
+        double actual = bobsAccount.getBalance();
+        assertEquals("should approve deposit and update balance", expected, actual, 0);
+    }
+
+    @Test
+    public void TransferTest() {
+        bank.transferFundsFromThisAccountToAnother(bobsAccount, princesAccount, 200);
+        double expectedBobBalance = 800;
+        double expectedPrinceBalance = 1200;
+        double actualBobBalance = bobsAccount.getBalance();
+        double actualPrinceBalance = princesAccount.getBalance();
+        assertEquals("bob's account should be debited", expectedBobBalance, actualBobBalance, 0);
+        assertEquals("prince's account should be credited", expectedPrinceBalance, actualPrinceBalance, 0);
+    }
 }
